4852b58559595594fa44cb8dedec1921192b6272
koitoresu <torreskoy@outlook.com>
2025-10-05 02:38:48 -0400

    Add initial RockPaperScissors game

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# RockPaperScissorsGame
-A simple Rock, Paper, Scissors game built in Python where players compete against the computer.
+# RockPaperScissorsGame 🎮
+
+A simple **Rock, Paper, Scissors** game built in Python where players compete against the computer.
+
+---
+
+## Features
+
+- Classic Rock, Paper, Scissors gameplay.
+- Track player HP and opponent HP.
+- Play until either the player or computer loses all HP.
+- Can be run as a Python script or standalone executable.
+
+---
+
+## Installation
+
+### Option 1: Run with Python
+
+1. Ensure Python 3 is installed: [Download Python](https://www.python.org/downloads/)
+2. Clone this repository or download `rps_game.py`.
+3. Run the game in your terminal or command prompt:
+
+```bash
+python rps_game.py
+```
--- src/RPS.py
+++ src/RPS.py
@@ -1,4 +1,4 @@
-from random import random
+from random import choice
 
 class RockPaperScissors(): 
     def __init__(self): 
@@ -6,22 +6,48 @@ class RockPaperScissors():
         self.opponentHp = 3
         self.playerOptions = ['Rock', 'Paper', 'Scissors'] 
 
-    def playGame(self): 
-        playerInput = str(input('What stance will you go with:\n'))
-        stance = playerInput.lower()
+    def opponentsChoice(self):
+        return choice(self.playerOptions)
+    
+    def evaluate_winner(self, choice1, choice2): 
+        if (choice1 == 'Rock' and choice2 == 'Scissors') or \
+            (choice1 == 'Paper' and choice2 == 'Rock') or \
+                (choice1 == 'Scissors' and choice2 == 'Paper'):
+            print('\nYOU WIN\n')
+            self.opponentHp -= 1
+        else:
+            print('\nYOU LOSE\n')
+            self.playerHp -= 1
 
-        while self.playerHp > 0: 
-            if len(playerInput) <= 0:
-                if stance not in self.playerOptions: 
-                    print('Your choice was invalid, please choose another stance!')
+    def playGame(self): 
+        while self.playerHp > 0 and self.opponentHp > 0: 
+            print(f'\tHP: {self.playerHp} | OpponentHp: {self.opponentHp}\n')
+            playerInput = str(input('What stance will you go with:\n'))
+            
+            if len(playerInput) == 0:
+                print('You must choose a stance\n')
+                continue
+                
+            stance = playerInput.capitalize()
+
+            if stance not in self.playerOptions: 
+                print('\nYour choice was invalid, please choose another stance!\n')
+                continue
 
             if self.opponentHp == 0:
-                print('WIINNER WINNER CHICKEN DINNER')
+                print('\nWINNER WINNER CHICKEN DINNER')
+                break
+            elif self.playerHp == 0: 
+                print('\nYOU LOSE!')
                 break
 
-    def opponentsChoice(self):
-        choice = random.random % 3
+            opponentsChoice = self.opponentsChoice()
 
-        return self.playerOptions[choice]
+            if stance == opponentsChoice: 
+                print('\nWE HAVE A TIE\n')
+            else: 
+                self.evaluate_winner(stance, opponentsChoice)
 
-
+if __name__ == '__main__':
+    initializeGame = RockPaperScissors()
+    initializeGame.playGame()
